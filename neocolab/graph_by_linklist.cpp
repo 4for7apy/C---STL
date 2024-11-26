6f4027559d3b016ff7f7f5011d2675d0dfc49bb9
arpit <arpit88407@gmail.com>
2024-11-27 00:26:58 +0530

    added post_order and pre_order

--- neocolab/graph_by_linklist.cpp
+++ neocolab/graph_by_linklist.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 
+
+
+// Pre Order - Root Left Right (RLR)
+// Inorder - Left Root Right (LRR)
+// Post Order - Left Right Root (LRR)
+
+int main(){
+
+}
 //Graph by linklist
 struct Node{
     int data;
@@ -15,7 +24,28 @@ int in_order(root, nodes){
         in_order(root.left, nodes);
     }
 }
-int main(){
 
+
+// pre order traversal
+int pre_order(root, nodes){
+    nodes.push(root.data);
+    if(root && root.left){
+        pre_order(root.left, nodes);
+    }
+    if(root && root.right){
+        pre_order(root.right, nodes);
+    }
+    return nodes;
 }
 
+
+int post_order(root, nodes){
+    if(root && root.left){
+        post_order(root.left, nodes);
+    }
+    if(root && root.right){
+        post_order(root.right, nodes);
+    }
+    nodes.push(root.data);
+    return nodes;
+}
