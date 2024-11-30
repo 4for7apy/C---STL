01ca4300850e04f9c2e16dac6b0c548d53119602
arpit <arpit88407@gmail.com>
2024-11-30 22:34:26 +0530

    second_maximum

---
+++ algorithm/array_easy_question/striver_2.cpp
@@ -0,0 +1,13 @@
+// Second Largest Element in an Array without Sorting
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solultion{
+    public:
+    int second_largest(vector<int>& arr){
+        int n = arr.size();
+        
+    }
+
+};
